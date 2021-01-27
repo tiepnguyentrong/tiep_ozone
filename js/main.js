2cb67ad3d81eb6639296c10e89c3a49fea558e3a
= <=>
2021-01-27 13:00:17 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-    nav:true,
-    items: 1,
-    dots: true,
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        dots: true,
 
-  });
+    });
+
 });
+$(window).on('load', function(event) {
+    $('body').removeClass('preloading');
+    $('.loader').delay(1000).fadeOut('fast');
+})
